282c22a7356bcb7415f84d6dda62d770119bd6d0
Asel <durusbekasela@gmail.com>
2024-10-08 17:33:18 +0600

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,29 +4,30 @@ import jm.task.core.jdbc.dao.*;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 public class UserServiceImpl implements UserService {
-    private UserDao userDaoHibernate = new UserDaoJDBCImpl();
+    UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
-        userDaoHibernate.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDaoHibernate.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDaoHibernate.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
+        System.out.println("User с именем - " + name +" " + lastName + " добавлен в базу данных" );
     }
 
     public void removeUserById(long id) {
-        userDaoHibernate.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDaoHibernate.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDaoHibernate.cleanUsersTable();
+        userDao.cleanUsersTable();
 
     }
 }
